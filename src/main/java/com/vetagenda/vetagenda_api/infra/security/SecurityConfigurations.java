1c02ae8b39f1a6883af9261864fc238bd9ec0c54
Gabriel Mendes <bielmendes2208@gmail.com>
2026-06-09 17:06:40 -0300

    Feat: Atualização no Security Configurations

--- src/main/java/com/vetagenda/vetagenda_api/infra/security/SecurityConfigurations.java
+++ src/main/java/com/vetagenda/vetagenda_api/infra/security/SecurityConfigurations.java
@@ -35,6 +35,8 @@ public class SecurityConfigurations {
                 .authorizeHttpRequests(authorize -> authorize
                         .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/veterinarios/**").hasAnyAuthority("ADMIN", "admin")
+                        .requestMatchers("api/usuarios/**").authenticated()
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
